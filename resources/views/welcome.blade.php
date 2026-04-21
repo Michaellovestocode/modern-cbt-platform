be561b68322a47712a97f491c77ef377bb0e1eb2
Crestedweb Michael <crestedweb@gmail.com>
2026-04-21 11:47:25 +0100

    Director picture added again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -459,7 +459,7 @@
              <!-- Staff Member 1 -->
             <div class="bg-white rounded-3xl shadow-lg overflow-hidden card-hover fade-in-up">
                 <div class="relative">
-    <img src="{{ asset('images/Director2.jpg') }}" alt="Director" class="w-full h-64 object-cover">
+    <img src="{{ asset('images/director2.jpg') }}" alt="Director" class="w-full h-64 object-cover">
     
     <div class="absolute top-4 right-4 bg-gradient-to-r from-blue-600 to-green-600 text-white px-3 py-1 rounded-full text-xs font-bold">
         Director
